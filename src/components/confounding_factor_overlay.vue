d0cf233f20246f676c3e7ff8f97909cae9de79a1
akleinau <56925445+akleinau@users.noreply.github.com>
2023-07-20 11:29:12 +0200

    improve confounding factor overlay
    improve confounding factor overlay

--- src/components/confounding_factor_overlay.vue
+++ src/components/confounding_factor_overlay.vue
@@ -10,10 +10,22 @@
         <v-card>
             <v-card-title class="mx-3">Confounding Factors</v-card-title>
             <v-card-text>
+                <div class="d-flex ml-2">
+                    <v-icon class="mr-2">mdi-help-circle-outline</v-icon>
+                    <span> Risk factor suggestions will consider if the observed
+                    risk increase can already be explained by the confounding factors. <br>
+                    Eg. Obesity increases the risk of diabetes.
+                    However, this increase is already explained by the correlation between obesity and bmi.
+                    </span>
+                </div>
                 <div v-for="item in confounding_factors" v-bind:key="item">
                     <v-checkbox v-model="item.factor" :label="item.name" @change="factor_changed(item)" hide-details/>
                 </div>
             </v-card-text>
+
+        <v-card-actions>
+            <v-btn @click="show = false">Close</v-btn>
+        </v-card-actions>
 
         </v-card>
     </v-dialog>
@@ -22,13 +34,15 @@
 <script>
 import {useDashboardStore} from "@/stores/dashboardStore";
 import {useRegressionStore} from "@/stores/regressionStore";
+import {useDataStore} from "@/stores/dataStore";
 
 export default {
     name: "confounding_factor_overlay",
     setup() {
         const dashboardStore = useDashboardStore()
         const regressionStore = useRegressionStore()
-        return {dashboardStore, regressionStore}
+        const dataStore = useDataStore()
+        return {dashboardStore, regressionStore, dataStore}
     },
     data() {
         return {
@@ -46,7 +60,7 @@ export default {
     },
     methods: {
         update_factors() {
-            this.confounding_factors = this.dashboardStore.dashboard_items.map(item => ({
+            this.confounding_factors = this.dashboardStore.dashboard_items.filter(item => item.name !== this.dataStore.target.name).map(item => ({
                 name: item.column.label,
                 column: item.column,
                 factor: this.dashboardStore.is_confounding_factor(item.column)
